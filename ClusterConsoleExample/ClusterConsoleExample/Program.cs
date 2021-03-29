d2f9fc62be582ce6d8b6c479f19d60810552201d
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-29 14:41:14 +0200

    Added projects for example

---
+++ ClusterConsoleExample/ClusterConsoleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClusterConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

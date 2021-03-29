94d6e8d09aae1855f925e8c5b7a66ea99ad5e277
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-29 14:52:31 +0200

    Added myAkkaConfig
    U ovom koraku smo dodali datoteku MyAkkaConfig i odgovarajuće klase za povezivanje appsettings.jsona sa objektom. Klasa nije dovršena, ali moramo instalirati nuget paket Newtonsofft.Json.

--- ClusterConsoleExample/SharedConfig/MyAkkaConfig.cs
+++ ClusterConsoleExample/SharedConfig/MyAkkaConfig.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 
 namespace SharedConfig
 {
     public class MyAkkaConfig
     {
+        [JsonProperty(PropertyName = "actor")]
+        public ActorConfig Actor { get; set; }
+        public class ActorConfig
+        {
+         
+            public class RemoteConfig
+            {
+                public class DotNettyConfig
+                {
+                    public class TcpConfig
+                    {
+                        
+                    }
+                }
+            }
+            
+            public class Cluster
+            {
+                
+            }
+        }
     }
 }

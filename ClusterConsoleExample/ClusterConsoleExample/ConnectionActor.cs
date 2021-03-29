c266a28889f0f001692a861040c5670138eeb237
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-29 15:20:27 +0200

    Capture IActorRef on MemberUp
    Kada se čvor u clusteru spoji, nemamo IActorRef. Poruke možemo slati preko ActorSeelctiona i to je ono sto napravimo u metodi SendIdentify
    Identify je ugrađena poruka na koju će svaki actor automatski odgovoriti porukom ActorIdentity.
    Jednom kada dobijemo odgovor od nekog actora na udaljenom čvoru, možemo spremiti njegov IActorRef u listu.

--- ClusterConsoleExample/ClusterConsoleExample/ConnectionActor.cs
+++ ClusterConsoleExample/ClusterConsoleExample/ConnectionActor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Akka.Actor;
 using Akka.Cluster;
 
@@ -5,10 +6,28 @@ namespace ClusterConsoleExample
 {
     public class ConnectionActor : ReceiveActor
     {
+        private List<IActorRef> _actorRefs;
         private Cluster _cluster = Cluster.Get(Context.System);
 
         public ConnectionActor()
         {
+            _actorRefs = new List<IActorRef>();
+
+            Receive<ClusterEvent.MemberUp>(up => SendIdentify(up));
+            Receive<ActorIdentity>(identity => AddActor());
+        }
+
+        private void SendIdentify(ClusterEvent.MemberUp up)
+        {
+            var rootPath = new RootActorPath(up.Member.Address);
+            var selection = Context.ActorSelection(rootPath);
+
+            selection.Tell(new Identify("1"));
+        }
+
+        private void AddActor()
+        {
+            _actorRefs.Add(Sender);
         }
 
         protected override void PreStart()
